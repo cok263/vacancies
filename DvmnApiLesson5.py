de372912c95e25715606882dd6a686e5272dbe9f
cok263 <cok26385@gmail.com>
2020-11-18 15:27:33 +0300

    print average salary for popular languages

--- DvmnApiLesson5.py
+++ DvmnApiLesson5.py
@@ -1,5 +1,6 @@
 import requests
 import pprint
+from collections import defaultdict
 
 
 def getAreaId(area_name):
@@ -26,6 +27,58 @@ def getSpecializationId(specialization_name):
     return -1
 
 
+def predict_rub_salary(vacancy):
+    if vacancy['salary'] is not None:
+        salary = vacancy['salary']
+        if salary['currency'] == 'RUR':
+            if salary['from'] is not None:
+                if salary['to'] is not None:
+                    return (int(salary['from']) + int(salary['to'])) / 2
+                else:
+                    return int(salary['from']) * 1.2
+            else:
+                return int(salary['to']) * 0.8
+    return None
+
+
+def popular_languages_info():
+
+    find_params = {
+        'area': getAreaId('Москва'),
+        'specialization': getSpecializationId('Программирование, Разработка'),
+        'period': 30,
+        'text': 'программист',
+    }
+
+    popular_languages = ('JavaScript', 'Java', 'Python',
+                         'Php', 'Ruby', 'C++', 'C', 'Go',)
+
+    languages_info = {}
+
+    for language in popular_languages:
+        language_info = defaultdict(int)
+
+        find_params['text'] = 'Программист {}'.format(language)
+        page_response = requests.get(url, params=find_params)
+        page_response.raise_for_status()
+        page_data = page_response.json()
+
+        for vacancy in page_data['items']:
+            predict_salary = predict_rub_salary(vacancy)
+            if predict_salary is not None:
+                language_info['average_salary'] += int(predict_salary)
+                language_info['vacancies_processed'] += 1
+
+        language_info['vacancies_found'] = page_data['found']
+        language_info['average_salary'] = int(
+            language_info['average_salary'] / language_info['vacancies_processed']
+        )
+        languages_info[language] = language_info
+    return languages_info
+
+
+
+
 base_url = 'https://api.hh.ru'
 url = '{}{}'.format(base_url, '/vacancies')
 
@@ -34,35 +87,18 @@ find_params = {
     'specialization': getSpecializationId('Программирование, Разработка'),
     'period': 30,
     'text': 'программист'
-
 }
 
-popular_languages = {
-    'JavaScript': 0,
-    'Java': 0,
-    'Python': 0,
-    'Php': 0,
-    'Ruby': 0,
-    'C++': 0,
-    'C': 0,
-    'Go': 0,
-}
 
 page_response = requests.get(url, params=find_params)
 page_response.raise_for_status()
 page_data = page_response.json()
 for vacancy in page_data['items']:
-    print(vacancy['name'], vacancy['salary'])
+    #print(vacancy['name'], vacancy['salary'])
+    print(vacancy['name'], predict_rub_salary(vacancy))
 
+pprint.pprint(popular_languages_info())
 
-for language in popular_languages:
-    find_params['text'] = 'Программист {}'.format(language)
-    page_response = requests.get(url, params=find_params)
-    page_response.raise_for_status()
-    page_data = page_response.json()
-    popular_languages[language] = page_data['found']
-
-pprint.pprint(popular_languages)
 
 
 
